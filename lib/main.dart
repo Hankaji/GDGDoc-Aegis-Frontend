0f85cf359f791e8a897911f835babd885f659942
Hankaji <hankaji135@gmail.com>
2025-04-07 11:25:14 +0700

    Setup again v1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:gdgdoc/screens/home/newcomer.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Hello World!')),
-        backgroundColor: Color(0xFFFF3080),
-      ),
+    return PlatformProvider(
+      builder:
+          (context) => PlatformTheme(
+            builder:
+                (context) => PlatformApp(
+                  localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+                    DefaultMaterialLocalizations.delegate,
+                    DefaultWidgetsLocalizations.delegate,
+                    DefaultCupertinoLocalizations.delegate,
+                  ],
+                  title: 'Aegis',
+                  home: Newcomer(),
+                ),
+          ),
     );
+
+    // return const MaterialApp(
+    //   title: "Aegis",
+    //   home: Scaffold(
+    //     body: Center(child: Text('Hello World!')),
+    //     backgroundColor: Color(0xFFFF3080),
+    //   ),
+    // );
   }
 }
